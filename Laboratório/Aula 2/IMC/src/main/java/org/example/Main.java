0f96e735045e9c33b2173205f6bab8e7b395ebcf
vivixsdutra <vitoria.dutra@ges.inatel.br>
2023-04-17 14:09:29 -0300

    Exercícios feitos em aula

---
+++ Laboratório/Aula 2/IMC/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Entrada de valores
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
---
+++ Laboratório/Aula 5/Conta/src/main/java/org/example/Titular.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Titular {
+    String nome;
+}
